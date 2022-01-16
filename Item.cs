452d6827f605f3b9626689d0a0a872748b6ac478
Korisnik <Korisnik@DESKTOP-NS5OI0U>
2022-01-16 20:33:52 +0100

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,15 @@
+﻿namespace OTTER
+{
+    //nepotrebna mi je ova klasa
+
+    //class Item : Sprite//isti sprite, mozda ga obrisem
+    //{
+
+
+    //    public Item(string s, int xk, int yk)
+    //      : base(s, xk, yk)
+    //    {
+
+    //    }
+    //}
+}
---
+++ Likovi.cs
@@ -0,0 +1,68 @@
+﻿namespace OTTER
+{
+    class Likovi : Sprite
+    {
+
+
+
+        //konstruktor
+        public Likovi(string s, int xk, int yk, int br)
+            : base(s, xk, yk)
+        {
+            this.Brzina = br;
+        }
+
+
+        private int brzina;
+
+
+        public int Brzina
+        {
+            get
+            {
+                return brzina;
+            }
+
+            set
+            {
+                brzina = value;
+            }
+        }
+
+
+
+    }
+    class Protivnik : Likovi
+    {
+        public Protivnik(string s, int xk, int yk)
+           : base(s, xk, yk, 7)
+        {
+
+        }
+    }
+    class GlavniLik : Likovi
+    {
+
+
+        public GlavniLik(string s, int xk, int yk)
+            : base(s, xk, yk, 5)
+        {
+
+        }
+        private int brojBodova;
+        public int BrojBodova
+        {
+            get
+            {
+                return brojBodova;
+            }
+
+            set
+            {
+                brojBodova = value;
+            }
+        }
+
+
+    }
+}
